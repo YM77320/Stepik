d244017023de067535ccfa77e1fcea6155ace2da
Air John <linx77320@gmail.com>
2024-08-22 20:15:15 +0300

    third edition

--- Main.java
+++ Main.java
@@ -9,8 +9,9 @@ public class Main {
             System.out.print(" " + n3);
             n1 = n2;
             n2 = n3;
-            // This is first changes is tarcfered to GitHub
-            // This is Second chsnged is added to file in Git 
+            // The first changes is tarcfered to GitHub.
+            // The Second chsnged is added to file in Git. 
+            //The third CHG is add to file from Git. 
         }
 
     }
